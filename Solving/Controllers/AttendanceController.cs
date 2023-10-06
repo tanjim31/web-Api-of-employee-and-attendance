42ea6f79be1f2cb54d74e1e13dd9323b75babf14
tanjim31 <rahatahmedtanjim1234@gmail.com>
2023-10-06 23:53:03 +0600

    add more code

--- Solving/Controllers/AttendanceController.cs
+++ Solving/Controllers/AttendanceController.cs
@@ -4,6 +4,11 @@ using Microsoft.EntityFrameworkCore;
 using Solving.DbCon;
 using Solving.Model;
 using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Web.Http;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using HttpPutAttribute = Microsoft.AspNetCore.Mvc.HttpPutAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace Solving.Controllers;
 
@@ -35,11 +40,70 @@ public class AttendanceController : Controller
     public async Task<ActionResult<Employeetb>> Get()
     {
         
-        var result= _context.employeetbs.OrderByDescending(e=>e.employeeSalary).Skip(2).First();
+        var result= _context.employeetbs.OrderByDescending(e=>e.employeeSalary).Skip(2).First();// employee who has 3rd salary
         return result;
     }
 
-    //// GET api/<CountryController>/5
+    public async Task<ActionResult<IEnumerable<dynamic>>> GetSalaryCombined()
+    {
+
+        var recordsFromTableA = _context.employeetbs.OrderByDescending(a => a.employeeSalary).ToList();
+
+        var recordsFromTableB = _context.attendances
+            .Where(b => b.isPresent == 1)
+            .OrderByDescending(b => b.isOffday)
+            .ToList();
+
+        //Combine the results
+       var combinedRecords = recordsFromTableA.Concat(recordsFromTableB).ToList();
+
+        return combinedRecords;
+
+    }
+
+
+    [HttpGet]
+    [Route("api/employeetbs/hierarchy/{employeeId}")]
+    public IHttpActionResult GetHierarchy(int employeeId)
+    {
+        var hierarchy = GetEmployeeHierarchy(employeeId);
+        if (hierarchy == null)
+        {
+            return IHttpActionResult(); // or another appropriate HTTP status code
+        }
+        return Ok(hierarchy);
+       
+
+    }
+    private IHttpActionResult IHttpActionResult()
+    {
+        throw new NotImplementedException();
+    }
+
+    private List<Employeetb> GetEmployeeHierarchy(int employeeId)
+    {
+        var hierarchy = new List<Employeetb>();
+
+        // Get the employee and their direct reports
+        var employee = _context.employeetbs.Find(employeeId);
+        if (employee != null)
+        {
+            hierarchy.Add(employee);
+
+            var directReports = _context.employeetbs.Where(e => e.supervisorId == employeeId).ToList();
+
+            foreach (var report in directReports)
+            {
+                // Recursively get the hierarchy for each direct report
+                hierarchy.AddRange(GetEmployeeHierarchy(report.employeeId));
+            }
+        }
+
+        return hierarchy;
+    }
+
+
+    //// GET api/<AttendanceController>
     [HttpGet("{employeeid}")]
     public async Task<ActionResult<Employeetb>> Get1(int employeeid)
     {
@@ -50,13 +114,14 @@ public class AttendanceController : Controller
 
     //// PUT api/<CountryController>/5
     [HttpPut("{id}")]
-    public async Task<ActionResult<Employeetb>> Put(int id, [FromBody] Employeetb employeetb)
+    public async Task<ActionResult<Employeetb>> Put(int id, Employeetb employeetb)
     {
         if (id != employeetb.employeeId)
         {
             return BadRequest();
         }
         _context.Entry(employeetb).State = EntityState.Modified;
+
         try
         {
             await _context.SaveChangesAsync();
